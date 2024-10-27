acc8da0ee605da45a4ba5cb6ca0b22dd32fe418d
Noel Austin <naustin00@gmail.com>
2024-10-27 02:49:10 -0400

    remove old records

--- gdb/main.go
+++ gdb/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "encoding/json"
-    "os"
+	"encoding/json"
+	"os"
+	"time"
+
+	log "github.com/sirupsen/logrus"
 )
 
+type UTCFormatter struct {
+    log.Formatter
+}
+
+func (u UTCFormatter) Format(e *log.Entry) ([]byte, error) {
+    e.Time = e.Time.UTC()
+    return u.Formatter.Format(e)
+}
 
 func filter(data []string, f func(string) bool) []string {
 
@@ -21,26 +31,56 @@ func filter(data []string, f func(string) bool) []string {
     return fltd
 }
 
-func main() {
+type LockItems []struct {
+    ResourceGroupName string `json:"resourceGroupName"`
+    TTLUnixEpoch      int64 `json:"ttl_unix_epoch"`
+}
 
-    type LockItems []struct {
-        ResourceGroupName string `json:"resourceGroupName"`
-        SkipLocking       string `json:"skipLocking"`
-        TTLUnitEpoch      string `json:"ttl_unit_epoch"`
-    }
-    
+func ReadGdbFile(gdb *LockItems) {
     file, err := os.ReadFile("db.json")
     if err != nil {
-     fmt.Println("Error reading josn file", err)
+        log.Panic(err)
     }
-    
-    var gdb LockItems
-    
+
     err = json.Unmarshal([]byte(file), &gdb)
     if err != nil {
-     fmt.Println("Error unmarshalling data", err)
+     log.Panic("Error unmarshalling gdb json data.", err)
      return
     }
-    fmt.Println(gdb)
+
+    log.Debug(gdb)
+}
+
+func RemoveOldRecords(gdb LockItems) LockItems {
+    currentTime := time.Now().Unix()
+    log.Debug("currenttime: ",currentTime)
+    var filteredItems LockItems
+
+    for _, item := range gdb {
+        if item.TTLUnixEpoch > currentTime {
+            filteredItems = append(filteredItems, item)
+        }
+    }
+
+    log.Debug(filteredItems)
+
+    return filteredItems
+}
+
+func main() {
+
+    log.SetLevel(log.DebugLevel)
+	log.SetReportCaller(true)
+    log.SetFormatter(UTCFormatter{&log.JSONFormatter{}})
+
+    log.Debug("Starting Main.")
+    
+    var gdb LockItems
+
+    ReadGdbFile(&gdb)
+
+    RemoveOldRecords(gdb)
     
 }
+
+
